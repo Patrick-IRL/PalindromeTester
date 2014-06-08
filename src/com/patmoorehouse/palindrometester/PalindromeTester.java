24a77ab87b1ad4e3708fd25c16b6f3bceddb83c3
Patrick <patmoorehouse.irl@gmail.com>
2014-06-08 22:35:15 +0100

    First Commit

---
+++ src/com/patmoorehouse/palindrometester/PalindromeTester.java
@@ -0,0 +1,47 @@
+package com.patmoorehouse.palindrometester;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class PalindromeTester {
+
+	public static void main(String[] args) throws IOException {
+		
+		String inFileName;
+		
+		if(args.length == 1){
+			inFileName = args[0];
+		}else{
+			inFileName = "words.txt";
+		}
+		
+		File inputFile = new File(inFileName);
+		BufferedReader in = new BufferedReader(new FileReader(inputFile));
+		
+		PalindromeTester pt = new PalindromeTester();
+		String line;
+		
+		while ((line = in.readLine()) != null) {
+			System.out.println(line + "\t\t" + pt.checkWord(line.toLowerCase()));
+		}
+		in.close();
+	}
+	
+	public boolean checkWord(String str){
+		int startIndex = 0;
+		int endIndex = str.length() - 1;
+		
+		while(startIndex <= endIndex){
+			if(!Character.isLetter(str.charAt(startIndex)) || (str.charAt(startIndex) != str.charAt(endIndex))){
+				return false;
+			}
+			
+			startIndex++;
+			endIndex--;
+		}
+		
+		return true;
+	}
+}
